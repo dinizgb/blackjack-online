19bea14e5900316c2f84f2a1b6c59a78055d8ca3
Gabriel Diniz <gabrieldiniz@Gabriels-MacBook-Air.local>
2025-04-03 23:45:25 -0300

    Feat: UI and logic improvements

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,24 @@
 // CSS
 import '../css/blackjack.css'
+import '../css/shared.css'
 import '../css/reset.css'
 
 // JS
-import { Blackjack } from '../js/blackjack.js'
+import { Blackjack } from '../js/blackjack/'
 
-document.querySelector('#app').innerHTML = `
-  <div class="container">
-        <h1>Blackjack</h1>
-        <div id="gameStatus"></div>
-        <div class="hand">
-            <h2>Dealer's Hand: <span id="dealerScore"></span></h2>
-            <div id="dealerHand"></div>
-        </div>
-        <div class="hand">
-            <h2>Player's Hand: <span id="playerScore"></span></h2>
-            <div id="playerHand"></div>
-        </div>
-        <div>
-            <button class="button" id="newGame">New Game</button>
-            <button class="button" id="hit" disabled>Hit</button>
-            <button class="button" id="stand" disabled>Stand</button>
-        </div>
-    </div>
-`
+// PAGES
+import { BlackjackView } from '../views/blackjack'
 
-// HANDLE BLACKJACK GAME
+document.querySelector('#app').innerHTML = BlackjackView
+
+// Game initialization
+/** @type {Blackjack} */
 const game = new Blackjack();
+
+/**
+ * Event listeners for game controls
+ */
 document.getElementById('newGame').addEventListener('click', () => game.startNewGame());
 document.getElementById('hit').addEventListener('click', () => game.hit());
 document.getElementById('stand').addEventListener('click', () => game.stand());
+document.getElementById('closeDialog').addEventListener('click', () => game.closeStatusDialog());
