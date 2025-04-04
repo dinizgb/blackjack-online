cb4cba54a2688f3aa376f644e4e287e379f7b5fb
Gabriel Diniz <gabrieldiniz@Gabriels-MacBook-Air.local>
2025-04-03 22:40:27 -0300

    Feat: Initial Blackjack game logic and styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,32 @@
-import '../css/style.css'
-import javascriptLogo from '../images/javascript.svg'
-import viteLogo from '../images/vite.svg'
-import { setupCounter } from './counter.js'
+// CSS
+import '../css/blackjack.css'
+import '../css/reset.css'
+
+// JS
+import { Blackjack } from '../js/blackjack.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+  <div class="container">
+        <h1>Blackjack</h1>
+        <div id="gameStatus"></div>
+        <div class="hand">
+            <h2>Dealer's Hand: <span id="dealerScore"></span></h2>
+            <div id="dealerHand"></div>
+        </div>
+        <div class="hand">
+            <h2>Player's Hand: <span id="playerScore"></span></h2>
+            <div id="playerHand"></div>
+        </div>
+        <div>
+            <button class="button" id="newGame">New Game</button>
+            <button class="button" id="hit" disabled>Hit</button>
+            <button class="button" id="stand" disabled>Stand</button>
+        </div>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+// HANDLE BLACKJACK GAME
+const game = new Blackjack();
+document.getElementById('newGame').addEventListener('click', () => game.startNewGame());
+document.getElementById('hit').addEventListener('click', () => game.hit());
+document.getElementById('stand').addEventListener('click', () => game.stand());
